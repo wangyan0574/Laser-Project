2a847a3d3b4ee3c3952bcd573abcf9ba2af9880f
wangyan0574 <youngyanw@outlook.com>
2020-04-09 02:39:50 -0400

    added calc_path for lasers and other useful function in finding block positions

--- Laser Project/Laser 4-5.py
+++ Laser Project/Laser 4-5.py
@@ -9,7 +9,7 @@ def load_bff(ftpr):
     f = open(ftpr, "r")
     bff = f.readlines()
     f.close()
-    grid = []
+    start_grid = []
     blk = [0] * 3
     pts = []
     lsr = []
@@ -29,12 +29,98 @@ def load_bff(ftpr):
                 pts.append([int(line[2]), int(line[4])])
             if line.startswith("L"):
                 laser = line.split()
-                lsr.append([int(laser[1]), int(laser[2]), int(laser[3]), int(laser[4])])
+                lsr.append([int(laser[3]), int(laser[4]), int(laser[1]), int(laser[2])])
 
     # initialized the grid into a 2d array
     for l in bff[start:stop]:
         l = l.replace("o", "1")
         l = l.replace("x", "0")
-        grid.append([int(x) for x in l.split()])
+        start_grid.append([int(x) for x in l.split()])
 
-    return grid, pts, blk, lsr
+    return start_grid, pts, blk, lsr
+
+
+def get_laserpos(pt1, pt2):
+    for i in (pt1[0], pt2[0]):
+        if (i % 2) != 0:
+            x = i
+    for j in (pt1[1], pt2[1]):
+        if (j % 2) != 0:
+            y = j
+    return x/2, y/2
+"""
+def check_pos(pt1, pt2, cur_grid):
+    x, y = get_laserpos(pt1, pt2)
+    if cur_grid(x, y):
+        return 0
+    else:
+        return 1
+"""
+def in_bound(x, y, grid):
+    if 0 <= x <= 2 * len(grid[0]) and 0 <= y <= 2 * len(grid):
+        return True
+    else:
+        return False
+
+
+def calc_path(start_x, start_y, start_vx, start_vy, grid, cur_grid):
+    path = []
+    x, y = start_x, start_y
+    vx, vy = start_vx, start_vy
+    while (in_bound(x, y, grid)) & (vx != 0):
+        cur_pt = x, y
+        path.append(x,y)
+        blk_x, blk_y = get_laserpos((x, y), (x + vx, y + vy))
+
+        if cur_grid(blk_x, blk_y) == "A":
+            if x - blk_x == 0:
+                vy = -vy
+            else:
+                vx = -vx
+        elif cur_grid(blk_x, blk_y) == "B":
+            vy = 0
+            vx = 0
+        else:
+            if x - blk_x == 0:
+                vy = -vy
+            else:
+                vx = -vx
+            for i,j in C.position
+            C.lsr = []
+        x, y = (x + vx, y + vy)
+
+
+
+
+class Block:
+    def __init__(self, blktype, position=(None, None)):
+        self.blktype = blktype
+        self.position = position
+
+
+    def get_position(self):
+        return self.position
+
+    def set_position(self, x, y):
+        self.position = (x, y)
+
+class Opaque(Block):
+
+class Laser:
+    def __init__(self, str_pos, str_dir):
+        self.str_pos = str_pos
+        self.str_dir = str_dir
+
+        self.CurPath = ()
+    def get_CurPath(self):
+        return self.CurPath
+    def set_CurPath(self):
+
+
+
+"""
+for i in xrange(0, len(prices)):
+    exec("price%d = %s" % (i + 1, repr(prices[i])));
+"""
+def main():
+
