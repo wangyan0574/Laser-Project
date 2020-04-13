bfd1ff25c20f130ee9926b4a737f8825211d01a5
siyu202 <55506398+siyu202@users.noreply.github.com>
2020-04-13 13:24:49 -0400

    Finished the program with all comments as well as updated the README.md

--- Laser Project/Lasersolver.py
+++ Laser Project/Lasersolver.py
@@ -3,9 +3,22 @@ EN.540.635 Software Carpentry
 Laser Project - Maze Generation and Solving
 Yan Wang & Siyu Chen
 """
+import time
 
 
 def load_bff(ftpr):
+    """
+    This will load the information of the board from the bff file
+
+    **Parameters**
+        ftpr: *str*, the string name of the file.
+    **Returns**
+        str_grid: *list*, 2d array, the starting grid of the board
+        pts: *list*, target points need to be crossed by laser/lasers
+        blk: *list*, kind and number of blocks we have for the board
+        lsr: *list*, the start point and start vx vy of the laser
+
+    """
     f = open(ftpr, "r")
     bff = f.readlines()
     f.close()
@@ -43,17 +56,41 @@ def load_bff(ftpr):
             if ele == "1":
                 split[i] = 1
         str_grid.append([x for x in split])
-
-    return str_grid, pts, blk, lsr
+    target = tuple(map(tuple, pts))
+    return str_grid, target, blk, lsr
 
 
 def get_laserpos(pt1, pt2):
+    """
+    This function will return the grid position of two adjacent points of 1 laser of 1 crossing
+    block. Using x,y and x+vx, y+vy: to get the grid position from half block increment to full
+    block increment. For example, the points are in a 8*8 grid, but the grid position are 4*4
+
+    **Parameters**
+        pt1: *list*, the xy of the current laser point (half increment, 8*8 style)
+        pt2: *list*, the xy of the next laser point (half increment, 8*8 style)
+    **Returns**
+        x: *int*, the x value of the crossing blk, (full block increment 4*4 style)
+        y: *int*, the y value of the crossing blk, (full block increment 4*4 style)
+
+    """
     x = (pt1[0] + pt2[0]) // 4
     y = (pt1[1] + pt2[1]) // 4
     return x, y
 
 
 def in_bound(blk_x, blk_y, grid):
+    """
+    This function will check if a block position (full, increment 4*4 style) is
+    in the boundary of the grid
+
+    **Parameters**
+        blk_x: *int*, the x of the current block position (full increment, 4*4 style)
+        blk_y: *int*, the y of the current block position (full increment, 4*4 style)
+    **Returns**
+        True or False
+
+    """
     if 0 <= blk_x <= len(grid[0]) - 1 and 0 <= blk_y <= len(grid) - 1:
         return True
     else:
@@ -61,21 +98,34 @@ def in_bound(blk_x, blk_y, grid):
 
 
 def calc_path(start_xy, start_vxvy, cur_grid, refract):
+    """
+    this function will calculate the path of the given laser: by storing its path in points (half
+    point increment 8*8 style). By given the current grid, the function will alternate this laser's
+    vx vy if it meet a block, either change direction (when hitting reflect); set vxvy to 0 (opaque);
+    or reflect and create a new one (refract)
+
+    **Parameters**
+        start_xy: *list*, the xy of this laser's starting point 8*8 style grid position
+        start_vxvy: *list*, the vx vy of this laser's starting direction (-1,-1) (1,-1) (1,1) or (-1,1)
+        cur_grid: *tuple*, contain the cur_grid information, with all blocks placed on the grid
+    **Returns**
+        path: *list* the path of the given laser:[[x,y], [x+ vx,y+vy], ...]
+
+    """
     path = []
+    refract.lsrPath = []
     x, y = start_xy
     vx, vy = start_vxvy
     blk_x, blk_y = get_laserpos((x, y), (x + vx, y + vy))
     path.append([x, y])
-    while (in_bound(blk_x, blk_y, cur_grid)) & (vx != 0):
-
+    while in_bound(blk_x, blk_y, cur_grid):
         if cur_grid[blk_y][blk_x] == "A":
             if x == blk_x * 2 + 1:
                 vy = -vy
             else:
                 vx = -vx
         elif cur_grid[blk_y][blk_x] == "B":
-            vy = 0
-            vx = 0
+            return path
         elif cur_grid[blk_y][blk_x] == "C":
             # we now introduce a new laser from this refract block. Its first element
             # will tell which refract block it belongs to. (The original laser will have
@@ -86,6 +136,10 @@ def calc_path(start_xy, start_vxvy, cur_grid, refract):
                 vy = -vy
             else:
                 vx = -vx
+        blk_x, blk_y = get_laserpos((x, y), (x + vx, y + vy))
+        if in_bound(blk_x, blk_y, cur_grid):
+            if cur_grid[get_laserpos((x, y), (x + vx, y + vy))[1]][get_laserpos((x, y), (x + vx, y + vy))[0]] == "A":
+                return path
         x, y = (x + vx, y + vy)
         blk_x, blk_y = get_laserpos((x, y), (x + vx, y + vy))
         path.append([x, y])
@@ -93,11 +147,15 @@ def calc_path(start_xy, start_vxvy, cur_grid, refract):
 
 
 class Block:
+    """
+    the block class, note reflect and opaque share the same class, but refract blocks will have
+    its own laser as its attribute.
+    """
+
     def __init__(self, total, blktype):
         self.total = total
         self.position = [0] * total
         self.blktype = blktype
-        self.cur_total = total
 
     def get_position(self):
         return self.position
@@ -110,6 +168,12 @@ class Refract(Block):
 
 
 class Laser:
+    """
+    the laser class, there is only 1 Laser object for this program, but the length of the
+    laser object may be more than 1 depend on the setup. Each their path will be stored
+    in the Path attribute.
+    """
+
     def __init__(self, str_xy, str_dir):
         self.str_xy = str_xy
         self.str_dir = str_dir
@@ -120,6 +184,18 @@ class Laser:
 
 
 def solve(ftpr):
+    """
+    this function will takes the bff file as input, it then initialize the values using the load_bff
+    function. Follow up by initialize all A, B, C block objects with their type and total number.
+    The laser is then initialized. It then run a couple of loops to try out all possible combination
+    of block setups, storing in set. Finally, it will validate the stored grids by calling the check
+    function to return the solution.
+
+    **Parameters**
+        ftpr: *str*, the name of the bff file
+    **Returns**
+        grid: *tuple*, the final solution of the grid
+    """
     # initializing all vars
     str_grid, pts, blk, lasers = load_bff(ftpr)
     A = Block(blk[0], "A")
@@ -157,25 +233,76 @@ def solve(ftpr):
                         new_comb.add(new_grid)
         combination = new_comb
 
-    for grid in combination:
-        if check_answer(grid, pts, lsr, C):
-            return grid
+    for comb in combination:
+        if check_answer(comb, pts, lsr, C):
+            return comb, str_grid
 
 
 def check_answer(cur_grid, pts, lsr, C):
+    """
+    this function will check the grid be given, calculate the lasers from the current grid. And
+    see if all the points are inside the laser path. Please note since we store the refract lasers
+    in the C object, we need to input C as well to see if any target points are hit by the C lsr.
+
+    the function will calulate the path of lasers by using the calc_path function, if a target pt
+    is in the path, it will remove that pt from the target array.
+
+    **Parameters**
+        cur_grid: *tuple*, 2d tuple that stores the current combination of blocks setup
+        pts: *list*, the points we want our laser to hit
+        lsr: *laser object*, the lsr object that we store the path
+        C: *refract block object*, the object we also store laser that may be created by refract
+            block
+    **Returns**
+        True / False, whether we find all target pts in the paths or not.
+
+    """
     target = [[c for c in r] for r in pts]
     for i in range(len(lsr.Path)):
         lsr.Path[i] = calc_path(lsr.str_xy[i], lsr.str_dir[i], cur_grid, C)
         for j in lsr.Path[i]:
             if j in target:
                 target.remove(j)
-
     if len(C.lsrPath) != 0:
         for i in C.lsrPath:
             if i in target:
                 target.remove(i)
-    return len(target) == 0
+    if len(target) == 0:
+        return True
+    else:
+        return False
+
+
+def save_sol(solved_grid, original_grid, bff):
+    name = bff.split(".")[0] + "_SOLVED.txt"
+    f = open(name, "w+")
+    f.write("Below is the solution for: "
+            + bff + "\n\n" + "Legend: \n1: free space" +
+            "\n0: unavailable space, no blocks can move here\n" +
+            "A: reflect block\nB: opaque block\n" +
+            "C: refract block\n\n" +
+            "Note: if there are fixed blocks in the original grid, the following\n" +
+            "      solved grid will present that fixed block to its type. Hence it\n" +
+            "      may appear to be more available blocks than the original.\n" +
+            "      Simply put the blocks accordingly!\n\n"
+            )
+
+    f.write("Original:\n")
+    for row in original_grid:
+        f.write(" ".join(str(s) for s in row) + '\n')
+    f.write("\nSolved:\n")
+    for row in solved_grid:
+        f.write(" ".join(str(s) for s in row) + '\n')
+    f.close()
 
 
 if __name__ == '__main__':
-    print(solve("mad_7.bff"))
+    print("\n*** Please enter the string name of the bff file name ***")
+    ftpr = input()
+    print("\n*** Please note some boards may take up to 18s to solve ***")
+    start_time = time.time()
+    solved, original = solve(ftpr)
+    save_sol(solved, original, ftpr)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print("*** Now please check the solved generated txt file :) ***")
--- Laser Project/README.md
+++ Laser Project/README.md
@@ -1,4 +1,54 @@
 # Laser-Project
 Laser group project for Software Carpenter JHU
 
-This project les!will solve the app game Lazors' puzz
+This project will solve the app game Lazors' puzzles
+
+HOW TO USE:
+
+    1. Be sure to have your bff file in the same directory (bff: board file format, explained below)
+    2. Simply load the program "Lazersolver.py" in your python IDE
+    3. After prompt, simply type the name of the file (including the file extension, this case is .bff): for example, tiny_5.bff
+    4. After some time, the program will output a txt file containing the original grid as well as the solved grid.
+    
+    
+BFF file:\
+Board file format is how we save the board information. (Check out any bff in the repo to know how they are formatted:) 
+Essentially, BFFs will include:
+    
+    1. The starting grid, with spaces in the form of
+        - "o" (a free space, all available blocks can move here)
+        - "x" (a filled space, non blocks can move here)
+        - "A" (a fixed reflect block, the white block)
+        - "B" (a fixed opaque block, the brown one)
+        - "C" (a fixed refract block, the transparent one)
+        ***just like the examples of bff in the repo, be sure to use spaces to separate the spaces types! 
+        And use "GRID START" and "GRID STOP" to indicate the grid style***
+     
+    2. What kind of blocks we will have and how many are there
+        - the line will start with "A" or "B" or "C", follow up by how many A or B or C is available for us to use
+    4. The laser
+        - the line will start with "L", follow by its starting position (in half increments, if a grid is 4 by 4, half increment style
+        will be 8*8), and direction: -1,-1 or 1,-1 or 1,1 or -1,1
+    3. The target pts we want to hit by the laser (also in half increment axis style)
+        - the line start with P, and follow by x and y position
+    Check out "mad_1.bff" to know more about the specific styling of bff file.
+    
+HOW TO READ THE OUTPUT TXT:\
+The output txt will be stored with "name + _SOLVED.txt" filename: for example, "tiny_5.bff" will have an output of "tiny_5_SOLVED.txt"\
+Inside the txt will have some legend info and contain the original grid as well as the solved grid (all in the same grid style of bff files):\
+
+***Please note the fixed blocks will also be present by the style name of it in the solved grid***: \
+(this means a fixed opaque block and a placed opaque will both be "B"), for example:
+
+***tiny_5***
+
+        Original:                    Solved:
+            1 B 1                       A B A
+            1 1 1                       1 1 1
+            1 1 1                       A C 1
+        
+Note: in the original grid we have a fixed opaque block ("B") at grid's row 1, col 2. It will be at the same location in the solved grid, too. \
+But we can certainly figure that out by just comparing the two.
+
+Thx, happy puzzling!
+    
